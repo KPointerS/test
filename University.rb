b61cb64b6fbb4c642065bc6f850991e42216aafc
KPointerS <2010kfupm@gmail.com>
2016-08-31 01:47:43 +0300

    Added initialize and summary

--- University.rb
+++ University.rb
@@ -1,5 +1,5 @@
 module Person
-	  attr_accessor :name, :id, :role
+	  attr_accessor :name, :p_id
   end
   
 class Instructor
@@ -12,7 +12,29 @@ end
 
 class Student
 	include Person
+	attr_accessor :gpa, :year, :courses
+	
+	def initialize (name = "", p_id = "0", gpa = 4.0, year= "Freshman")
+		@name =  name
+		@p_id = p_id
+		@gpa = gpa
+		@year = year
+	end
+	
+	def summary
+		puts name
+		puts "ID: #{p_id}"
+		puts "He is a #{year} with gpa: #{gpa} out of 4"
+	end
+	
 end
 
 
-print "hello world"
+
+
+
+student1 = Student.new("John smith", "200956482", 3.2, "Senior")
+student1.summary
+
+
+
