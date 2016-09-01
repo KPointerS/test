b6a55dbcdaf0f542eea607c56a1c96b8e1acfd5d
KPointerS <2010kfupm@gmail.com>
2016-09-01 18:12:00 +0300

    readwrite file

--- io_test.rb
+++ io_test.rb
@@ -2,3 +2,10 @@ output_file = File.new('output.txt', 'w')
 output_file.puts "Me: Hello world"
 output_file.print "World: Hi"
 output_file.close
+
+#reading from the file above and reversing words
+File.open('output.txt', 'r') do |file|
+while line = file.gets
+  puts "** " + line.chomp.reverse + " **"
+  end
+end
